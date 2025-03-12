99af7b038ae05b2b729620281073eb92fa81a4a9
Doddi Sahasra <sahasradoddi@gmail.com>
2025-03-12 12:10:50 +0530

    design template added

---
+++ resetpassword.py
@@ -0,0 +1,9 @@
+user_name=input("enter useer name :")
+
+password=input("enter password:")
+
+if user_name=="Admin" and password=="Admin":
+    print("You can reset")
+else:
+    print("you cannot reset")
+    
